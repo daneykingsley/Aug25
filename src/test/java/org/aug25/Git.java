32c5dd26a93845c6062b8d1590102064b792cc60
daneykingsley <daneykingsley@gmail.com>
2022-08-25 17:13:57 +0530

    added 3 methods by emp 2

--- src/test/java/org/aug25/Git.java
+++ src/test/java/org/aug25/Git.java
@@ -15,16 +15,16 @@ public class Git {
 		System.out.println("3");
 
 	}
-	private void m4() {
-		System.out.println("4");
+	private void m7() {
+		System.out.println("7");
 
 	}
-	private void m5() {
-		System.out.println("5");
+	private void m8() {
+		System.out.println("8");
 
 	}
-	private void m6() {
-		System.out.println("6");
+	private void m9() {
+		System.out.println("9");
 
 	}
 	
